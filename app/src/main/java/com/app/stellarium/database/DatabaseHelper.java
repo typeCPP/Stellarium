59fff0a3cdcfee43747127b29d78c69d8a50ac35
Андрей Усанов <iambreaddd@mail.ru>
2022-01-25 13:13:09 +0300

    Add dropping database on update

--- app/src/main/java/com/app/stellarium/database/DatabaseHelper.java
+++ app/src/main/java/com/app/stellarium/database/DatabaseHelper.java
@@ -25,6 +25,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + InformationTable.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 }
