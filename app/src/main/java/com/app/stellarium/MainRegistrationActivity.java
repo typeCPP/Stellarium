bd3a73d6e02bbd7fb2f004a8153332780b31c913
Danil-Pashkov <taverna201@gmail.com>
2022-02-10 15:18:05 +0300

    Add main registration page

---
+++ app/src/main/java/com/app/stellarium/MainRegistrationActivity.java
@@ -0,0 +1,138 @@
+package com.app.stellarium;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.percentlayout.widget.PercentLayoutHelper;
+import androidx.percentlayout.widget.PercentRelativeLayout;
+
+public class MainRegistrationActivity extends AppCompatActivity {
+
+    private boolean isSigninScreen = true;
+    private TextView tvSignupInvoker;
+    private LinearLayout llSignup;
+    private TextView tvSigninInvoker;
+    private LinearLayout llSignin;
+    private Button btnSignup;
+    private Button btnSignin;
+    private Animation scaleUp;
+    Button twitterButton, facebookButton, googleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_registration);
+
+        tvSignupInvoker = findViewById(R.id.tvSignupInvoker);
+        tvSigninInvoker = findViewById(R.id.tvSigninInvoker);
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignin = findViewById(R.id.btnSignin);
+
+        twitterButton = findViewById(R.id.buttonTwitter);
+        googleButton = findViewById(R.id.buttonGoggle);
+        facebookButton = findViewById(R.id.buttonFacebook);
+
+        llSignup = findViewById(R.id.llSignup);
+        llSignin = findViewById(R.id.llSignin);
+        scaleUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_up);
+        class ButtonClickListener implements View.OnClickListener {
+            @Override
+            public void onClick(View view) {
+
+                switch (view.getId()) {
+                    case (R.id.buttonFacebook):
+                        view.startAnimation(scaleUp);
+                        break;
+                    case (R.id.buttonTwitter):
+                        view.startAnimation(scaleUp);
+                        break;
+                    case (R.id.buttonGoggle):
+                        view.startAnimation(scaleUp);
+                        break;
+                    case (R.id.btnSignup):
+                        view.startAnimation(scaleUp);
+                        break;
+                    case (R.id.btnSignin):
+                        view.startAnimation(scaleUp);
+                        break;
+
+                }
+            }
+        }
+
+        facebookButton.setOnClickListener(new ButtonClickListener());
+        twitterButton.setOnClickListener(new ButtonClickListener());
+        googleButton.setOnClickListener(new ButtonClickListener());
+        btnSignin.setOnClickListener(new ButtonClickListener());
+        btnSignup.setOnClickListener(new ButtonClickListener());
+
+        tvSignupInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSigninScreen = false;
+                showSignupForm();
+            }
+        });
+
+        tvSigninInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSigninScreen = true;
+                showSigninForm();
+            }
+        });
+        showSigninForm();
+
+    }
+
+    private void showSignupForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.15f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.85f;
+        llSignup.requestLayout();
+
+        tvSignupInvoker.setVisibility(View.GONE);
+        tvSigninInvoker.setVisibility(View.VISIBLE);
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_right_to_left);
+        llSignup.startAnimation(translate);
+
+        Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_right_to_left);
+        btnSignup.startAnimation(clockwise);
+
+    }
+
+    private void showSigninForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.85f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.15f;
+        llSignup.requestLayout();
+
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_left_to_right);
+        llSignin.startAnimation(translate);
+
+        tvSignupInvoker.setVisibility(View.VISIBLE);
+        tvSigninInvoker.setVisibility(View.GONE);
+        Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_left_to_right);
+        btnSignin.startAnimation(clockwise);
+    }
+}
