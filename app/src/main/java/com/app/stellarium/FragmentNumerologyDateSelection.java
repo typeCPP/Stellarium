4345a3745f0ea626bc3c3e49bae53e66bacf84ec
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-04-01 19:17:49 +0300

    Add view for loading window, delete comments in RegistrationActivity, fix vibration in FragmentYesOrNo

--- app/src/main/java/com/app/stellarium/FragmentNumerologyDateSelection.java
+++ app/src/main/java/com/app/stellarium/FragmentNumerologyDateSelection.java
@@ -7,6 +7,8 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -31,7 +33,10 @@ import com.app.stellarium.utils.ServerConnection;
 import com.app.stellarium.utils.jsonmodels.Numerology;
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.Calendar;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 public class FragmentNumerologyDateSelection extends Fragment {
 
@@ -45,6 +50,7 @@ public class FragmentNumerologyDateSelection extends Fragment {
     private Bundle bundle;
     private LinearLayout layoutDate;
     private boolean isSetDate;
+    private LoadingDialog loadingDialog;
     Animation scaleUp;
 
     public static FragmentPythagoreanSquareDateSelection newInstance(String param1, String param2) {
@@ -66,13 +72,14 @@ public class FragmentNumerologyDateSelection extends Fragment {
         MainActivity activity = (MainActivity) getActivity();
         if (activity != null)
             activity.setNumberOfPrevFragment();
-
+        loadingDialog = new LoadingDialog(requireContext());
 
         scaleUp = AnimationUtils.loadAnimation(getContext(), R.anim.scale_up);
         isSetDate = false;
         editTextDate = view.findViewById(R.id.numerologic_date_selection);
         layoutDate = view.findViewById(R.id.numerologic_date_layout_1);
         nextButton = view.findViewById(R.id.nextNumerologicButton);
+        LayoutInflater layoutInflater = getLayoutInflater();
         nextButton.setAlpha(0f);
         nextButton.setVisibility(View.INVISIBLE);
         bundle = new Bundle();
@@ -84,26 +91,35 @@ public class FragmentNumerologyDateSelection extends Fragment {
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
                     view.startAnimation(scaleUp);
                     int numerologyNumber = calculateNumber(birthdayDay, birthdayMonth, birthdayYear);
-                   /* LoadingDialog loadingDialog = new LoadingDialog(FragmentNumerologyDateSelection.this);
-                    loadingDialog.startLoadingDialog();
+                    loadingDialog.show();
+                    loadingDialog.startGifAnimation();
+                    loadingDialog.stopGifAnimation();
+                    loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
                     Handler handler = new Handler();
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
                             getServerResponseToDatabase(numerologyNumber);
+                            try {
+                                Thread.sleep(50000);
+                            } catch (InterruptedException ignored) {
+
+                            }
                             handler.post(new Runnable() {
                                 @Override
                                 public void run() {
                                     Fragment fragment = new FragmentNumerology();
                                     bundle.putInt("numerologyNumber", numerologyNumber);
                                     fragment.setArguments(bundle);
-                                    loadingDialog.dismissLoadingDialog();
+//                                    loadingDialog.dismiss();
                                     getParentFragmentManager().beginTransaction().setCustomAnimations(R.animator.fragment_alpha_in, R.animator.fragment_alpha_out, R.animator.fragment_alpha_in, R.animator.fragment_alpha_out)
                                             .addToBackStack(null).replace(R.id.frameLayout, fragment).commit();
                                 }
                             });
                         }
-                    }).start();*/
+                    }).start();
                 }
                 return true;
             }
--- app/src/main/java/com/app/stellarium/dialog/LoadingDialog.java
+++ app/src/main/java/com/app/stellarium/dialog/LoadingDialog.java
@@ -1,19 +1,17 @@
 package com.app.stellarium.dialog;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
-import com.app.stellarium.FragmentAffirmation;
 import com.app.stellarium.R;
 
 import java.io.IOException;
@@ -22,14 +20,17 @@ import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
 public class LoadingDialog extends Dialog implements android.view.View.OnClickListener {
-    private Activity activity;
-    private Fragment fragment;
-    private AlertDialog dialog;
     private GifImageView gifImageView;
     private GifDrawable gifDrawable;
+    private TextView text;
+    private LinearLayout linearLayoutError;
+    private Context context;
+    private Button button;
 
     public LoadingDialog(@NonNull Context context) {
         super(context);
+        this.context = context;
+        this.setCancelable(false);
     }
 
     @SuppressLint("ResourceType")
@@ -38,12 +39,22 @@ public class LoadingDialog extends Dialog implements android.view.View.OnClickLi
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.loading_dialog);
-       gifImageView=findViewById(R.id.gif_loading_dialog);
-        try {
-            gifDrawable=new GifDrawable(getContext().getResources(), R.id.gif_loading_dialog);
-        } catch (IOException e) {
-            e.printStackTrace();
+        gifImageView = findViewById(R.id.gif_loading_dialog);
+        button = findViewById(R.id.button_error_loading_dialog);
+
+        gifDrawable = (GifDrawable) gifImageView.getDrawable();
+        text = findViewById(R.id.text_dialog);
+        linearLayoutError = findViewById(R.id.layout_dialog_error);
+        linearLayoutError.setAlpha(0f);
+
+        class LoadingDialogOnClickListener implements View.OnClickListener {
+
+            @Override
+            public void onClick(View view) {
+                startGifAnimation();
+            }
         }
+        button.setOnClickListener(new LoadingDialogOnClickListener());
     }
 
 
@@ -52,11 +63,21 @@ public class LoadingDialog extends Dialog implements android.view.View.OnClickLi
 
     }
 
-    public void startGifAnimation(){
+    public void startGifAnimation() {
+        text.setVisibility(View.VISIBLE);
+        text.animate().alpha(1f).setDuration(250).setListener(null);
+        linearLayoutError.animate().alpha(0f).setDuration(250).setListener(null);
+        linearLayoutError.setVisibility(View.INVISIBLE);
         gifDrawable.start();
     }
 
-    public void stopAnimation(){
-        gifDrawable.stop();
+    public void stopGifAnimation() {
+        if (gifDrawable.isRunning()) {
+            text.animate().alpha(0f).setDuration(250).setListener(null);
+            text.setVisibility(View.INVISIBLE);
+            linearLayoutError.setVisibility(View.VISIBLE);
+            linearLayoutError.animate().alpha(1f).setDuration(250).setListener(null);
+            gifDrawable.stop();
+        }
     }
 }
