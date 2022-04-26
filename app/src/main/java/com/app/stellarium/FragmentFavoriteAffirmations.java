f9ca8e00240af9a3d5274b02ac95c25d3160258d
prlwk <molchanova2.aa@edu.spbstu.ru>
2022-04-26 19:30:52 +0300

    Fix favorite affirmations and main activity

--- app/src/main/java/com/app/stellarium/FragmentFavoriteAffirmations.java
+++ app/src/main/java/com/app/stellarium/FragmentFavoriteAffirmations.java
@@ -137,6 +137,7 @@ public class FragmentFavoriteAffirmations extends Fragment {
             AffirmationPagerAdapter pagerAdapter = new AffirmationPagerAdapter(pages);
             ViewPager viewPager = new ViewPager(getContext());
             viewPager.setAdapter(pagerAdapter);
+            viewPager.setOverScrollMode(View.OVER_SCROLL_NEVER);
             viewPager.setCurrentItem(1);
             return viewPager;
         } else {
