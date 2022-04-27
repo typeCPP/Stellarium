7160bdd2e73eb438ebc79fc102b11d880b4c30cb
Андрей Усанов <iambreaddd@mail.ru>
2022-04-27 21:42:56 +0300

    Make password encode with salt

--- app/src/main/java/com/app/stellarium/utils/PasswordEncoder.java
+++ app/src/main/java/com/app/stellarium/utils/PasswordEncoder.java
@@ -6,8 +6,11 @@ import java.security.MessageDigest;
 public class PasswordEncoder {
     public static String encodePasswordMD5(String password) {
         try {
+            String salt = "jiFNCqikRSnqstPS7wso";
             MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] message = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            md.update(password.getBytes(StandardCharsets.UTF_8));
+            byte[] message = md.digest();
             StringBuilder sb = new StringBuilder();
             for (byte b : message) {
                 sb.append(String.format("%02x", b));
